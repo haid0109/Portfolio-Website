b094200981aad9cf83bbc9268a78f630bdbae31f
haid0109 <51113567+haid0109@users.noreply.github.com>
2019-11-08 13:10:51 +0100

    fixed problem with navbar not showing

--- js/navigationBar.js
+++ js/navigationBar.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop() > 50) { $('nav').css('background', 'rgba(30,33,36,1)'); }
-        else { $('nav').css('background', 'rgba(30,33,36,0)'); }
+    $(window).scroll(function darkenNavbar(){
+        if($(window).scrollTop() === 0) { $('nav').css('background', 'rgba(30,33,36,0)'); }
+        else { $('nav').css('background', 'rgba(30,33,36,1)'); }
     });
 
     $(".navbar-toggler").click(function () {
         $('nav').css('background', 'rgba(30,33,36,1)');
         $('nav').css('text-align', 'center');
     });
+
+    $(this).on('scroll click', function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $('.navbar').on('hidden.bs.collapse', function () {
+        if($(window).scrollTop() === 0) { $('nav').css('background', 'rgba(30,33,36,0)'); }
+        else { $('nav').css('background', 'rgba(30,33,36,1)'); }
+    })  
 });
